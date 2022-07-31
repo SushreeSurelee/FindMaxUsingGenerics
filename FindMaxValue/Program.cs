309e3e38eb91512016edf7bd1b619218b299d191
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-01 05:15:03 +0530

    [Sushree | Added fuctionality to to find maximun value out of any 3 string values.]

--- FindMaxValue/Program.cs
+++ FindMaxValue/Program.cs
@@ -12,12 +12,12 @@ namespace FindMaxValue
         {
             Console.WriteLine("Wlcome to Find maximum number Application.");
             Console.WriteLine("\nPlease choose number as shown below to find maximum value");
-            Console.WriteLine("\n1:Find maximum number for Integer values\n2.Find maximum number for Float values");
+            Console.WriteLine("\n1:Find maximum value for integer inputs\n2:Find maximum value for float inputs\n3:Find maximum value for string inputs");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
-                    Console.WriteLine("\nEnter the First, Second and Third Integer value in below line.");
+                    Console.WriteLine("\nEnter the three Integer value in below line.");
                     int a = Convert.ToInt32(Console.ReadLine());
                     int b = Convert.ToInt32(Console.ReadLine());
                     int c = Convert.ToInt32(Console.ReadLine());
@@ -26,13 +26,22 @@ namespace FindMaxValue
                     Console.WriteLine("{0} is the maximum value out of {1} {2} {3}", FindMaximumValue.FindMaxValue(a, b, c),a,b,c);
                     break;
                 case 2:
-                    Console.WriteLine("\nEnter the First, Second and Third Integer value in below line.");
+                    Console.WriteLine("\nEnter the three Float value in below line.");
                     float d = Convert.ToSingle(Console.ReadLine());
                     float e = Convert.ToSingle(Console.ReadLine());
                     float f = Convert.ToSingle(Console.ReadLine());
                     float maxFloat = FindMaximumValue.FindMaxValue(d, e, f);
                     if (maxFloat != 0)
                         Console.WriteLine("{0} is the maximum value out of {1} {2} {3}", FindMaximumValue.FindMaxValue(d, e, f), d, e, f);
+                    break;
+                case 3:
+                    Console.WriteLine("\nEnter the three string value in below line.");
+                    string g = Console.ReadLine();
+                    string h = Console.ReadLine();
+                    string i = Console.ReadLine();
+                    string maxString = FindMaximumValue.FindMaxValue(g, h, i);
+                    if (maxString != null)
+                        Console.WriteLine("{0} is the maximum value out of {1} {2} {3}", FindMaximumValue.FindMaxValue(g, h, i), g, h, i);
                     break;
             }
             Console.ReadLine();
