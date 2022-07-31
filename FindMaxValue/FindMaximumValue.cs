c1c7194a73aa26dfca957c7693fd4c88a822f626
“Sushree” <sushreesureleesahu98@gmail.com>
2022-08-01 04:27:29 +0530

    [Sushree | Added functionality to find maximun value out of any 3 integer.]

---
+++ FindMaxValue/FindMaximumValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxValue
+{
+    class FindMaximumValue
+    {
+        public static int FindMaxValue(int first,int second,int third)
+        {
+            if(first.CompareTo(second)>0 && first.CompareTo(third)>0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("2 or 3 of the given number might be having same value.");
+                return 0;
+            }
+        }
+    }
+}
---
+++ FindMaxValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wlcome to Find maximum number Application.");
+            Console.WriteLine("\nPlease choose number as shown below to find maximum value");
+            Console.WriteLine("\n1:Find maximum number for integer values");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter the First, Second and Third Integer value in below line.");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    int second = Convert.ToInt32(Console.ReadLine());
+                    int third = Convert.ToInt32(Console.ReadLine());
+                    int max = FindMaximumValue.FindMaxValue(first, second, third);
+                    if(max!=0)
+                    Console.WriteLine("{0} is the maximum value out of {1} {2} {3}", FindMaximumValue.FindMaxValue(first, second, third),first,second,third);
+                    break;
+            }
+            Console.ReadLine();
+         }   
+                
+    }
+}
